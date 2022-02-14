4d614ce8976e74160d831bf9277dad54a92b2f0a
bane.shi <fengxiaotx@163.com>
2022-02-15 00:47:05 +0800

    hh

--- rest-conf/src/main/java/com/ms/rest/conf/swagger/SwaggerConfiguration.java
+++ rest-conf/src/main/java/com/ms/rest/conf/swagger/SwaggerConfiguration.java
@@ -1,50 +1,43 @@
 package com.ms.rest.conf.swagger;
 
-import io.swagger.models.auth.In;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.SecurityScheme;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
-import static com.google.common.net.HttpHeaders.AUTHORIZATION;
-
-@EnableOpenApi
 @Configuration
 public class SwaggerConfiguration {
+
     @Bean
-    public Docket swagger() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("default")
-                .apiInfo(new ApiInfoBuilder().title("api").version("1.0.0").build())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ms.rest"))
-                .build()
-                .securitySchemes(securitySchemes());
+    public GroupedOpenApi backendApi() {
+        return GroupedOpenApi.builder()
+                .group("IT-HERE Backend API")
+                .pathsToMatch("/api/**")
+                .packagesToScan("net.ithere.rest.backend")
+                .addOpenApiCustomiser(authorizationOpenApiCustomiser())
+                .build();
     }
 
     @Bean
-    public Docket swaggerAdmin() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("bossRest")
-                .apiInfo(new ApiInfoBuilder().title("boss api").version("1.0.0").build())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ms.bossrest"))
-                .build()
-                .securitySchemes(securitySchemes());
+    public GroupedOpenApi bossApi() {
+        return GroupedOpenApi.builder()
+                .group("IT-HERE Boss API")
+                .pathsToMatch("/boss-api/**")
+                .packagesToScan("net.ithere.rest.boss")
+                .addOpenApiCustomiser(authorizationOpenApiCustomiser())
+                .build();
     }
 
-    private List<SecurityScheme> securitySchemes() {
-        List<SecurityScheme> parameters = new ArrayList<>();
-        parameters.add(new ApiKey("BASE_TOKEN", AUTHORIZATION, In.HEADER.toValue()));
-        return parameters;
+    public OpenApiCustomiser authorizationOpenApiCustomiser() {
+        return openApi -> openApi.schemaRequirement("AUTHORIZATION", new SecurityScheme().type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name(AUTHORIZATION))
+                .addSecurityItem(new SecurityRequirement().addList("AUTHORIZATION"))
+                ;
     }
 
 }
