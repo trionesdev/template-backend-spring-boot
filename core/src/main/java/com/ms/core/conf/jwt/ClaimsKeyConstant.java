c15b50e38c70f1891177c242cbd3a25d57ce2c89
bane.shi <fengxiaotx@163.com>
2022-04-09 18:15:09 +0800

    hh

--- core/src/main/java/com/ms/core/conf/jwt/ClaimsKeyConstant.java
+++
@@ -1,6 +0,0 @@
-package com.ms.core.conf.jwt;
-
-public class ClaimsKeyConstant {
-    public static final String ACTOR_ID = "ACTOR_ID";
-    public static final String ACTOR_ROLE = "ACTOR_ROLE";
-}
---
+++ infrastructure/src/main/java/com/ms/infrastructure/conf/jwt/JwtFacade.java
@@ -0,0 +1,64 @@
+package com.ms.infrastructure.conf.jwt;
+
+
+import com.google.common.collect.Maps;
+import com.moensun.commons.context.actor.ActorRoleEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static com.ms.infrastructure.conf.jwt.ClaimsKeyConstant.ACTOR_ID;
+import static com.ms.infrastructure.conf.jwt.ClaimsKeyConstant.ACTOR_ROLE;
+
+
+@RequiredArgsConstructor
+@Configuration
+@EnableConfigurationProperties(value = {
+        JwtProperties.class
+})
+public class JwtFacade {
+
+    private final JwtProperties jwtProperties;
+
+    public String generateUser(Object userId) {
+        if (Objects.isNull(userId)) {
+            return null;
+        }
+        Map<String, Object> claims = Maps.newHashMap();
+        claims.put(ACTOR_ID, userId);
+        claims.put(ACTOR_ROLE, ActorRoleEnum.USER.name());
+        return JwtUtils.generateToken(jwtProperties, String.valueOf(userId), claims);
+    }
+
+    public String generateBossUser(Object userId) {
+        if (Objects.isNull(userId)) {
+            return null;
+        }
+        Map<String, Object> claims = Maps.newHashMap();
+        claims.put(ACTOR_ID, userId);
+        claims.put(ACTOR_ROLE, ActorRoleEnum.BOSS_USER.name());
+        return JwtUtils.generateToken(jwtProperties, String.valueOf(userId), claims);
+    }
+
+    public String generate(Object userId, String role) {
+        if (Objects.isNull(userId)) {
+            return null;
+        }
+        Map<String, Object> claims = Maps.newHashMap();
+        claims.put(ACTOR_ID, userId);
+        claims.put(ACTOR_ROLE, role);
+        return JwtUtils.generateToken(jwtProperties, String.valueOf(userId), claims);
+    }
+
+    public String generate(String subject, Map<String, Object> claims) {
+        return JwtUtils.generateToken(jwtProperties, subject, claims);
+    }
+
+    public Map<String, Object> parse(String token) {
+        return JwtUtils.parseClaims(jwtProperties, token);
+    }
+
+}
