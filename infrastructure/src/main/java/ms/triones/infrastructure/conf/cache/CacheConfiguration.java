0fa99a711a7fa671b43a69c9cad0281fbd42b303
bane.shi <fengxiaotx@163.com>
2023-10-24 18:59:18 +0800

    feat:

--- infrastructure/src/main/java/ms/triones/infrastructure/conf/cache/CacheConfiguration.java
+++ infrastructure/src/main/java/ms/triones/infrastructure/conf/cache/CacheConfiguration.java
@@ -1,23 +1,22 @@
 package ms.triones.infrastructure.conf.cache;
 
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -27,9 +26,10 @@ import java.util.Objects;
 @Configuration
 @EnableCaching
 @RequiredArgsConstructor
-@EnableConfigurationProperties({CacheProperties.class})
+@EnableConfigurationProperties({CacheExtProperties.class})
 public class CacheConfiguration {
     private final CacheProperties cacheProperties;
+    private final CacheExtProperties cacheExtProperties;
     private final ObjectMapper objectMapper;
 
     @Bean
@@ -46,53 +46,68 @@ public class CacheConfiguration {
     }
 
     @Bean
-    public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate, StringRedisTemplate stringRedisTemplate) {
-        RedisCacheManager.RedisCacheManagerBuilder cacheManagerBuilder = RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(Objects.requireNonNull(redisTemplate.getConnectionFactory()))
-                .cacheDefaults(defaultRedisCacheConfiguration(cacheProperties.getDefaultTtl()))
-                .transactionAware();
-        if (MapUtils.isNotEmpty(cacheProperties.getCacheNames())) {
-            cacheProperties.getCacheNames().forEach((k, v) -> {
-                RedisCacheConfiguration cacheConfiguration;
-                switch (v.getValueSerializer()) {
-                    case JSON:
-                        cacheConfiguration = buildCacheConfiguration(redisTemplate, v.getTtl());
-                        break;
-                    case STRING:
-                    default:
-                        cacheConfiguration = buildCacheConfiguration(stringRedisTemplate, v.getTtl());
-                        break;
-                }
-                cacheManagerBuilder.withCacheConfiguration(k, cacheConfiguration);
-            });
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(
+            RedisTemplate<String, Object> redisTemplate,
+            StringRedisTemplate stringRedisTemplate
+    ) {
+        return (builder) -> {
+            builder.cacheDefaults(defaultRedisCacheConfiguration());
+            if (MapUtils.isNotEmpty(cacheExtProperties.getCacheNames())) {
+                cacheExtProperties.getCacheNames().forEach((k, v) -> {
+                    RedisCacheConfiguration cacheConfiguration;
+                    switch (v.getValueSerializer()) {
+                        case JSON:
+                            cacheConfiguration = buildCacheConfiguration(redisTemplate, v);
+                            break;
+                        case STRING:
+                        default:
+                            cacheConfiguration = buildCacheConfiguration(stringRedisTemplate, v);
+                            break;
+                    }
+                    builder.withCacheConfiguration(k, cacheConfiguration);
+                });
+
+            }
+        };
+    }
+
+    private <T> RedisCacheConfiguration buildCacheConfiguration(RedisTemplate<String, T> redisTemplate, CacheExtProperties.CacheItem item) {
+        RedisCacheConfiguration redisCacheConfiguration = defaultRedisCacheConfiguration();
+        redisCacheConfiguration.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getStringSerializer()));
+        redisCacheConfiguration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+        if (Objects.nonNull(item.getTimeToLeave())) {
+            redisCacheConfiguration.entryTtl(Duration.ofSeconds(item.getTimeToLeave()));
         }
-        return cacheManagerBuilder.build();
+        return redisCacheConfiguration;
     }
 
-    private <T> RedisCacheConfiguration buildCacheConfiguration(RedisTemplate<String, T> redisTemplate, Long seconds) {
-        return RedisCacheConfiguration
-                .defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getStringSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()))
-                .disableCachingNullValues()
-                .entryTtl(Duration.ofSeconds(seconds));
-    }
-
-    private RedisCacheConfiguration defaultRedisCacheConfiguration(Long seconds) {
-
-        return RedisCacheConfiguration
+    private RedisCacheConfiguration defaultRedisCacheConfiguration() {
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
                 .defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(redisObjectMapper())))
-                .disableCachingNullValues()
-                .entryTtl(Duration.ofSeconds(seconds));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(redisObjectMapper())));
+        if (StrUtil.isNotBlank(redisProperties.getKeyPrefix())) {
+            redisCacheConfiguration = redisCacheConfiguration.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+        if (Objects.nonNull(redisProperties.getTimeToLive())) {
+            redisCacheConfiguration = redisCacheConfiguration.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            redisCacheConfiguration = redisCacheConfiguration.disableKeyPrefix();
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            redisCacheConfiguration = redisCacheConfiguration.disableCachingNullValues();
+        }
+        return redisCacheConfiguration;
     }
 
-    private ObjectMapper redisObjectMapper(){
+    private ObjectMapper redisObjectMapper() {
         ObjectMapper objMapper = objectMapper.copy();
         objMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objMapper.activateDefaultTyping(objMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
         return objMapper;
     }
+
 
 }
