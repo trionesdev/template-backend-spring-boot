3cc2f37e48b2a696c398d886cc1b59578871cce2
bane.shi <fengxiaotx@163.com>
2024-05-05 18:10:18 +0800

    feat: add entity

---
+++ domain/src/main/java/com/trionesdev/mes/domain/core/domains/manufacture/dao/entity/ManufactureProcess.java
@@ -0,0 +1,23 @@
+package com.trionesdev.mes.domain.core.domains.manufacture.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.trionesdev.commons.mybatisplus.entity.BaseLogicEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "manufacture_process")
+public class ManufactureProcess extends BaseLogicEntity {
+    @TableId(type = IdType.ASSIGN_ID)
+    private String id;
+    private String tenantId;
+}
---
+++ domain/src/main/java/com/trionesdev/mes/domain/core/domains/manufacture/dao/mapper/ManufactureProcessMapper.java
@@ -0,0 +1,7 @@
+package com.trionesdev.mes.domain.core.domains.manufacture.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.trionesdev.mes.domain.core.domains.manufacture.dao.entity.ManufactureProcess;
+
+public interface ManufactureProcessMapper extends BaseMapper<ManufactureProcess> {
+}
