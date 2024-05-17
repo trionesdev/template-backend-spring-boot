5e2401b3a2f584d38b1783068873d738f2743165
bane.shi <fengxiaotx@163.com>
2024-05-17 23:48:28 +0800

    feat: temp save code

--- interface/rest-backend/src/main/java/com/trionesdev/mes/rest/backend/domains/masterdata/controller/ro/ProductDefinitionUpdateRO.java
+++ interface/rest-backend/src/main/java/com/trionesdev/mes/rest/backend/domains/masterdata/controller/ro/ProductDefinitionUpdateRO.java
@@ -2,7 +2,21 @@ package com.trionesdev.mes.rest.backend.domains.masterdata.controller.ro;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
 @Data
 public class ProductDefinitionUpdateRO {
+    @NotBlank
     private String name;
+    private String unitId;
+    private String specification;
+    @NotBlank
+    private String type;
+    private String technologicalCode; //工艺路线
+    private String defaultSupplierCode;
+    private Integer maxInventory;
+    private Integer minInventory;
+    private BigDecimal unitCost; //成本单价
+    private BigDecimal unitPrice; //销售单价
 }
