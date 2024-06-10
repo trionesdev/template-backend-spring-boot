51f42536e6db909699e59a8c15c88f4427357b4e
bane.shi <fengxiaotx@163.com>
2024-06-10 23:37:32 +0800

    feat: temp save code

---
+++ domain/src/main/java/com/trionesdev/mes/domain/core/dto/manufacture/ManufactureOrderTaskDTO.java
@@ -0,0 +1,24 @@
+package com.trionesdev.mes.domain.core.dto.manufacture;
+
+import com.trionesdev.mes.domain.core.dto.masterdata.DefectiveDTO;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+@Data
+public class ManufactureOrderTaskDTO {
+    private String orderCode;
+    private String processCode;
+    private String name;
+    private BigDecimal ratio;
+    private BigDecimal planQuantity;
+    private BigDecimal goodQuantity;
+    private BigDecimal defectiveQuantity;
+    private List<DefectiveDTO> defectives;
+    private Instant planStartTime;
+    private Instant planEndTime;
+    private Instant actualStartTime;
+    private Instant actualEndTime;
+}
