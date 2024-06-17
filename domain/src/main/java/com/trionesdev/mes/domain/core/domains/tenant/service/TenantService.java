706f890c84ed36800cbd64c50fe2fc750a11adbf
bane.shi <fengxiaotx@163.com>
2024-06-17 23:24:08 +0800

    feat: temp save code

--- domain/src/main/java/com/trionesdev/mes/domain/core/domains/tenant/service/TenantService.java
+++ domain/src/main/java/com/trionesdev/mes/domain/core/domains/tenant/service/TenantService.java
@@ -1,4 +1,8 @@
 package com.trionesdev.mes.domain.core.domains.tenant.service;
 
+import com.trionesdev.mes.domain.core.domains.tenant.entity.TenantMember;
+
 public interface TenantService {
+
+    void createTenantMember(TenantMember tenantMember);
 }
