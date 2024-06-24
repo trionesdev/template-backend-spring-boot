200d54175e18d7560edfd6e51f95b37976c630aa
Bane.Shi <fengxiaotx@163.com>
2024-06-24 17:58:27 +0800

    feat: init

--- domain/src/main/java/com/trionesdev/mes/domain/core/domains/user/manager/impl/UserManager.java
+++ domain/src/main/java/com/trionesdev/mes/domain/core/domains/user/manager/impl/UserManager.java
@@ -1,19 +1,30 @@
 package com.trionesdev.mes.domain.core.domains.user.manager.impl;
 
+import com.trionesdev.commons.exception.DuplicatedException;
+import com.trionesdev.mes.domain.core.domains.user.entity.User;
+import com.trionesdev.mes.domain.core.domains.user.internal.UserBeanConvert;
 import com.trionesdev.mes.domain.core.domains.user.repository.impl.UserRepository;
 import com.trionesdev.mes.domain.core.domains.user.repository.po.UserPO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserManager {
+    private final UserBeanConvert convert;
     private final UserRepository userRepository;
 
-    public void createUser(UserPO user) {
-        userRepository.save(user);
+    public String createUser(User user) {
+        var userPhoneSnap = userRepository.selectByPhone(user.getPhone());
+        if (Objects.nonNull(userPhoneSnap)) {
+            throw new DuplicatedException("PHONE_EXISTS");
+        }
+        var userPo = convert.entityToPO(user);
+        userRepository.save(userPo);
+        return userPo.getId();
     }
 
     public void deleteUserById(String id) {
@@ -34,6 +45,17 @@ public class UserManager {
 
     public Optional<UserPO> findUserByPhone(String phone) {
         return Optional.ofNullable(userRepository.selectByPhone(phone));
+    }
+
+    public String bindUser(User user) {
+        return Optional.ofNullable(userRepository.selectByPhone(user.getPhone()))
+                .map(UserPO::getId)
+                .orElseGet(() -> {
+                    var userPo = convert.entityToPO(user);
+                    userRepository.save(userPo);
+                    return userPo.getId();
+                });
+
     }
 
 }
