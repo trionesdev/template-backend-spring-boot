dfb0cc4922e0aa1f65d8acb6a636fa4f2fa27fcc
Bane.Shi <fengxiaotx@163.com>
2024-06-25 18:17:07 +0800

    feat: init

---
+++ infrastructure/src/main/java/com/trionesdev/mes/infrastructure/ddd/AggregateRoot.java
@@ -0,0 +1,6 @@
+package com.trionesdev.mes.infrastructure.ddd;
+
+import java.io.Serializable;
+
+public interface AggregateRoot<ID extends Serializable> {
+}
---
+++ infrastructure/src/main/java/com/trionesdev/mes/infrastructure/ddd/Entity.java
@@ -0,0 +1,6 @@
+package com.trionesdev.mes.infrastructure.ddd;
+
+import java.io.Serializable;
+
+public interface Entity<ID extends Serializable> {
+}
